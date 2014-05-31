52eccd6e52d41d569b5d18ce752d7529247f178d
Zoltán Borsos <zolli07@gmail.com>
2014-05-31 22:46:05 +0200

    *Change: Fully refactored for Facebook PHP SDK 4.0
    *Change: Added some helper methods
    *Change: More useful comments in configuration

--- src/Zolli/Phacebook/Phacebook.php
+++ src/Zolli/Phacebook/Phacebook.php
@@ -1,6 +1,10 @@
 <?php namespace Zolli\Phacebook;
 
-use Facebook;
+use Facebook\FacebookRedirectLoginHelper;
+use Facebook\FacebookRequest;
+use Facebook\FacebookSession;
+use Facebook\FacebookRequestException;
+use Facebook\GraphObject;
 use Config;
 use Response;
 use URL;
@@ -9,90 +13,160 @@ use Input;
 
 /**
  * Class Phacebook
+ * Facebook API helper class for Laravel 4
  *
  * @package Zolli\Phacebook
+ * @author Zoltan Borsos <zolli07@gmail.com>
+ * @copyright 2014 Zoltan Borsos
+ * @license MIT
+ * @version 2.0
  */
 class Phacebook {
 
     /**
-     * Az inícializált facebook osztály példánya
-     *
-     * @var Facebook
+     * @var FacebookRedirectLoginHelper
      */
-    private $facebook = null;
+    private $loginHelper = null;
 
     /**
-     * A authentikáció során megkapott accessToken
-     *
-     * @var string
+     * @var FacebookSession
      */
-    private $accessToken = null;
+    private $facebookSession = null;
 
     /**
-     * Konstruktor
+     * Constructor
+     *
+     * Initialize the facebook application helper and the redirector
      */
     public function __construct() {
         $this->initFacebook();
+        $this->tryGetSession();
     }
 
     /**
-     * Inícializálj a Facebook osztályta a configban megadott beállításokkal
+     * Start the session (not the laravel Sessionmanager class)
+     * and set the redirect URL on the redirector
      */
     private function initFacebook() {
-        $this->facebook = new Facebook(Config::get('Phacebook::applicationConfig'));
+        session_start();
+
+        FacebookSession::setDefaultApplication(Config::get("Phacebook::applicationConfig.appId"), Config::get("Phacebook::applicationConfig.secret"));
+        $this->loginHelper = new FacebookRedirectLoginHelper(URL::to(Config::get("Phacebook::redirectUrl")));
     }
 
     /**
-     * Visszaadja az inícializált facebook osztályt
-     *
-     * @return Facebook|null Null ha még nincs inícializálva valamiért
+     * Try to get the session, and initialize a new FacebookSession class
      */
-    public function getFacebook() {
-        return $this->facebook();
+    private function tryGetSession() {
+        if (isset($_SESSION) && isset($_SESSION['fb_token'])) {
+            $this->facebookSession = new FacebookSession($_SESSION['fb_token']);
+
+            try {
+                if (!$this->facebookSession->validate()) {
+                    $this->facebookSession = null;
+                }
+            } catch (Exception $e) {
+                $this->facebookSession = null;
+            }
+        }
     }
 
+    /**
+     * Handles the Facebook callback request
+     */
+    public function handleCallback() {
+        try {
+            $this->facebookSession = $this->loginHelper->getSessionFromRedirect();
+            $_SESSION['fb_token'] = $this->facebookSession->getToken();
+
+            //Little workaround, because Laravel Redierct facade not works as expected in package
+            Response::make('',302)->header('Location',(string)Config::get("Phacebook::redirectAfterLoginUrl"))->send();
+        } catch(FacebookRequestException $ex) {
+            var_dump($ex);
+        } catch(Exception $e) {
+            var_dump($e);
+        }
+    }
+
+    /**
+     * Get the initialized Facebook session class
+     *
+     * @return FacebookSession
+     */
+    public function getSession() {
+        return $this->facebookSession;
+    }
+
+    /**
+     * Get the logged in user default data's
+     *
+     * @return GraphObject
+     */
     public function getUser() {
-        return $this->facebook->api("/me");
+        return (new FacebookRequest($this->facebookSession, 'GET', '/me'))->execute()->getGraphObject();
     }
 
     /**
-     * Lefuttat egy FQL kérést és mindenképpen aláírja az eccessToken-el
+     * Runs an FQL query and return its content
      *
-     * @param string $query A kérés amit futtatni akarunk
-     * @return array
+     * @param String $query A valid FQL query
+     * @param String $castAs Cast the result as a specialized GraphObject (GraphAlbum, GraphLocation, GraphUser)
+     * @return GraphObject
      */
-    public function executeFQLQuery($query) {
-        $params = [
-            'method' => 'fql.query',
-            'access_token' => $this->accessToken,
-            'query' => $query,
-        ];
+    public function executeFQLQuery($query, $castAs = NULL) {
+        if($castAs === NULL) {
+            $castAs = GraphObject::className();
+        }
 
-        return $this->facebook->api($params);
+        return (new FacebookRequest($this->facebookSession, 'GET', '/fql', [
+            'access_token' => $this->facebookSession->getToken(),
+            'q' => $query,
+        ]))->execute()->getGraphObject($castAs);
     }
 
     /**
-     * Elvégz az authentikációt
+     * Post a message to user wall, optional with a link
+     *
+     * @param $message
+     * @param string $link
+     * @return int The new post ID
+     */
+    public function postToWall($message, $link = "") {
+        $response = (new FacebookRequest($this->facebookSession, 'POST', '/me/feed', [
+            'message' => $message,
+            'link' => $link,
+        ]))->execute()->getResponse();
+
+        return $response->getProperty('id');
+    }
+
+    /**
+     * Make a raw request with unique parameters and url
+     *
+     * @param String $path The API endpoint
+     * @param arra $params Parameters for the request
+     * @param string $requestType The request HTTP verb
+     *
+     * @return \Facebook\FacebookResponse
+     */
+    public function makeRawRequest($path, $params, $requestType = 'GET') {
+        return (new FacebookRequest($this->facebookSession, $requestType, $path, $params))->execute();
+    }
+
+    /**
+     * Start an authentication request
+     *
+     * @return bool True if user is already logged in, otherwise redirect to the login URL
      */
     public function authenticate() {
-        if($this->facebook->getUser() == 0) {
-            $url = $this->facebook->getLoginUrl([
-                "redirect_uri" => URL::to(Config::get('Phacebook::redirectUrl')),
-                "scope" => implode(',', Config::get('Phacebook::scopes')),
-            ]);
+        $this->tryGetSession();
 
-            //Kis heggesztés egy bug miatt ami megakadályozza a Redirector osztály megflelő használatát
+        if($this->facebookSession == null) {
+            $url = $this->loginHelper->getLoginUrl(Config::get('Phacebook::scopes'));
             Response::make('',302)->header('Location',(string)$url)->send();
         } else {
             return TRUE;
         }
-    }
-
-    public function handleCallback() {
-        $code = Input::get('code');
-        $this->accessToken = $this->facebook->getAccessToken();
-        $this->facebook->getUser();
-        Response::make('',302)->header('Location',(string)Config::get("Phacebook::redirectAfterLoginUrl"))->send();
     }
 
 }
--- src/config/config.php
+++ src/config/config.php
@@ -2,27 +2,54 @@
 
 return [
 
-    /**
-     * Ide kerül a kapcsolt alkalmazás azonosítására szolgáló adatok
-     */
+    /*
+    |--------------------------------------------------------------------------
+    | The Facebook application credentials
+    |--------------------------------------------------------------------------
+    |
+    | Paste here the Facebook application ID and the application
+    | secret key to use this class for authentication purposes
+    |
+    | (Note, these pre-filled credentials provides access for a test application)
+    |
+    */
     'applicationConfig' => [
         'appId' => '671607966227507',
         'secret' => '6545f6ee10d38abdabc12f917755e2f5',
     ],
 
-    /**
-     * Az URL amire visszajön a facebook callback
-     */
+    /*
+    |--------------------------------------------------------------------------
+    | Redirect URL
+    |--------------------------------------------------------------------------
+    |
+    | You need to add a "Website platform" to the Facebook application
+    | This field must be sam as "Site URL" field (without base URL) in the
+    | application configuration
+    |
+    */
     'redirectUrl' => '/facebook-callback',
 
-    /**
-     * Hova irányítson vissza miután sikeresen megtörtént az authentikáció
-     */
+    /*
+    |--------------------------------------------------------------------------
+    | Application login URL
+    |--------------------------------------------------------------------------
+    |
+    | This is the base URL. Will be redirected the user after the login
+    | is successful
+    |
+    */
     'redirectAfterLoginUrl' => '/',
 
-    /**
-     * A scopok emit elkérünk a usertől auth során
-     */
+    /*
+    |--------------------------------------------------------------------------
+    | Application scopes
+    |--------------------------------------------------------------------------
+    |
+    | Here you can specify the scopes (or permissions) witch will be asking
+    | from the user on first contact
+    |
+    */
     'scopes' => ['email'],
 
 ];
